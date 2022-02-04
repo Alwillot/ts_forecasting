954bddada8fe63b332e16c3933a115c5d8681977
AleWillot <awillot@quantmetry.com>
2022-02-04 17:06:00 +0100

    Import function

--- scripts/imports.py
+++ scripts/imports.py
@@ -1,5 +1,20 @@
+import os
+
 import numpy as np
 import pandas as pd
 
-def import_csv():
-    
+def import_csv(folder_path: str, filename: str, **kwargs):
+    """
+    Imports a csv file given its folder and name
+
+    Args:
+        folder_path (str): Name of the folder in which the csv file is
+        filename (str): Name of the file, should end with a '.csv'
+    """
+    path_joined = os.path.join(folder_path, filename)
+    return pd.read_csv(path_joined, **kwargs)
+
+
+
+if __name__ == "__main__":
+    print(import_csv("data/", "oil.csv").head(10))
